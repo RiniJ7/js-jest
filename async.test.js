c42b1248b0924d0b779140de30790ddf0512b728
rinij <rinimani7@gmail.com>
2023-11-24 22:58:48 -0700

    use async and await in test script

---
+++ async.test.js
@@ -0,0 +1,17 @@
+
+//use async and await in test script
+
+test('the data is peanut butter', async () => {
+    const data = await fetchData();
+    expect(data).toBe('peanut butter');
+  });
+  
+  test('the fetch fails with an error', async () => {
+    expect.assertions(1);
+    try {
+      await fetchData();
+    } catch (e) {
+      expect(e).toMatch('error');
+    }
+  });
+
